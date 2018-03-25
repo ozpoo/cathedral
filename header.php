4f0ff9b038a1174a525d6abf213b12cea34fe61c
ozpoo <hello@ericandren.com>
2018-03-24 17:03:13 -0700

    N

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 		<link href="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png?v=2" rel="apple-touch-icon-precomposed">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<meta name="format-detection" content="telephone=no">
 		<meta name="keywords" content="<?php echo get_option( 'key_words', 'Default Text Optional' ); ?>">
